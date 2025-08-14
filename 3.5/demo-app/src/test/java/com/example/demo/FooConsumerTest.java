2f3eebee43b7b524ec1ba8bae99b86ae6c096d36
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2025-08-14 17:58:14 +0200

    Proper user of ImportAutoConfiguration

--- 3.5/demo-app/src/test/java/com/example/demo/FooConsumerTest.java
+++ 3.5/demo-app/src/test/java/com/example/demo/FooConsumerTest.java
@@ -1,17 +1,18 @@
 package com.example.demo;
 
 import com.example.library.Bar;
-import com.example.library.EnableBarTest;
+import com.example.library.BarAutoConfiguration;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest(classes = {AppConfig.class, FooConfig.class}, properties = {"debug=true"})
-@EnableBarTest
+@SpringBootTest(classes = AppConfig.class, properties = {"debug=true"})
+@ImportAutoConfiguration({BarAutoConfiguration.class, FooAutoConfiguration.class})
 public class FooConsumerTest {
 
     @MockitoSpyBean
